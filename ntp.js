8e2d40b6783944b4b128dcb0cf6ac96c51a357f7
skythrilla <schuyler.lawrence@gmail.com>
2021-05-12 13:44:28 -0700

    add class

--- ntp.js
+++ ntp.js
@@ -5,10 +5,10 @@ const mySites = document.querySelector('.mySites');
 const myAdded = document.querySelector('.myAdded');
 
 const mySitesObj = [
-  // {
-  //   url: 'https://www.twitch.tv/directory/following/live/',
-  //   imgSrc: 'img/twitch.png',
-  // },
+  {
+    url: 'https://www.twitch.tv/directory/following/live/',
+    imgSrc: 'img/twitch.png',
+  },
   { url: 'https://music.amazon.com', imgSrc: 'img/music.png' },
   { url: 'https://watch.spectrum.net/guide', imgSrc: 'img/tv.jpeg' },
   {
@@ -30,7 +30,7 @@ const mySitesObj = [
   { url: 'https://hangouts.google.com', imgSrc: 'img/hangouts.png' },
   { url: 'https://www.youtube.com', imgSrc: 'img/youtube.png' },
   { url: 'https://maps.google.com', imgSrc: 'img/maps.png' },
-  // { url: 'https://www.google.com', imgSrc: 'img/google.jpeg' },
+  { url: 'https://www.google.com', imgSrc: 'img/google.jpeg' },
 ];
 // mySites
 for (const [key, value] of Object.entries(mySitesObj)) {
@@ -49,23 +49,6 @@ if (select !== null) locStrgArr = select.split(',');
 
 locStrgArr.sort();
 
-// myAdded
-for (let i = 0; i < locStrgArr.length; i++) {
-  let node = document.createElement('div');
-  let word = locStrgArr[i];
-  node.id = 'theDiv';
-  node.innerHTML = `<a href='${word}' rel = 'noopener noreferrer' id = 'aLink'> <img class='newLink' src='img/link.png' /> ${word}</a><button value=${word} id='itemBtn${i}'>x</button>`;
-  myAdded.appendChild(node);
-  document.querySelector('#itemBtn' + i).addEventListener('click', (e) => {
-    e.preventDefault();
-    let found = locStrgArr.findIndex((a) => a === e.target.value);
-    locStrgArr.splice(found, 1);
-    locStrgArr.length === 0
-      ? myStorage.clear()
-      : myStorage.setItem('selectionsList', locStrgArr);
-    node.remove();
-  });
-}
 addBtn.addEventListener('click', (event) => {
   event.preventDefault();
   let word = inpt.value;
@@ -100,3 +83,72 @@ clearBtn.addEventListener('click', (event) => {
   myStorage.clear();
   inpt.value = '';
 });
+
+class Link {
+  constructor(url, idx, removeFn) {
+    this.url = url;
+    this.index = idx;
+    this.removeLink = removeFn;
+  }
+
+  getUrl() {
+    if (!this.url.includes('http')) {
+      this.url = `https://${this.url}`;
+    }
+
+    return this.url;
+  }
+
+  buildLink() {
+    const a = document.createElement('a');
+    const url = this.getUrl();
+    a.href = url;
+
+    const imgg = document.createElement('img');
+    imgg.classList = 'newLink';
+    imgg.src = 'img/link.png';
+    a.appendChild(imgg);
+    a.rel = 'noopener noreferrer';
+    a.id = 'aLink';
+    a.innerHTML += ` ${url}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'x';
+    removeButton.addEventListener('click', () => this.removeLink(this.index));
+
+    const wrapper = document.createElement('div');
+    wrapper.id = 'theDiv';
+    wrapper.appendChild(a);
+    wrapper.appendChild(removeButton);
+    return wrapper;
+  }
+}
+
+class LinkController {
+  constructor(urls) {
+    this.el = document.createElement('div');
+    // this.el.id = 'theDiv';
+    this.urls = urls;
+    this.renderLinks();
+    myAdded.appendChild(this.el);
+  }
+
+  renderLinks = () => {
+    this.el.innerHTML = ''; // clear out to avoid duplicates
+    this.links = this.urls.map((url, idx) => {
+      const link = new Link(url, idx, this.removeLink);
+      this.el.appendChild(link.buildLink());
+      return link;
+    });
+  };
+
+  removeLink = (index) => {
+    this.urls = this.urls.filter((_, i) => i !== index); // remove index passed
+    locStrgArr.length === 0
+      ? myStorage.clear()
+      : myStorage.setItem('selectionsList', this.urls);
+    this.renderLinks();
+  };
+}
+
+new LinkController(locStrgArr);
